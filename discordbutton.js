8a51029213ac761abb65c0d0863e6bcf5b81954b
ppsn <60221587+ppsn19@users.noreply.github.com>
2022-11-28 20:23:03 +0200

    new stuff

---
+++ discordbutton.js
@@ -0,0 +1,5 @@
+function showNotice() {
+    navigator.clipboard.writeText("ppsn#3568");
+    cn = document.getElementById("copyNotice")
+    cn.innerHTML = "Discord handle copied to clipboard!"
+}
---
+++ readme.txt
@@ -0,0 +1,2 @@
+background: https://gfycat.com/hopefulbrightdragonfly-iroduku-sekai-no-ashita-kara-anime
+font: https://www.dafont.com/hashiba.font
